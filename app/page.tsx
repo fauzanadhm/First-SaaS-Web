c55656ef745dab89570f7956f3f4751752ad9f92
adhimaaf <fauzanadhimaandi@gmail.com>
2025-10-08 09:16:21 +0800

    implement popular companions list

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,45 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import CompanionCard from '@/components/CompanionCard';
+import CompanionsList from '@/components/CompanionsList';
+import CTA from '@/components/CTA';
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Welcome to my SaaS Web</h1>
-      <Button>Let's get started</Button>
-    </div>
-  )
-}
+    <main>
+      <h1 className="text-2xl">Popular Companions</h1>
 
-export default Page
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject='science'
+          duration={45}
+          color='#ffda6e'
+        />
+        <CompanionCard
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject='math'
+          duration={30}
+          color='#e5d0ff'
+        />
+        <CompanionCard
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="English Literature"
+          subject='language'
+          duration={45}
+          color='#bde7ff'
+        />
+      </section>
+
+      <section>
+        <CompanionsList></CompanionsList>
+        <CTA></CTA>
+      </section>
+    </main>
+  );
+};
+
+export default Page;
